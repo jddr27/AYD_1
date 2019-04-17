07c0ce948462f4fd02b4ea736b8695ff940a6469
jddr27 <jddeleonr.inf2013@gmail.com>
2019-04-17 00:17:09 -0600

    actualizacion_modelos
    encriptador y usuarios

---
+++ Carrito_Compras/Carrito_Compras/Models/Encriptador.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Carrito_Compras.Models
+{
+    public class Encriptador
+    {
+
+        public string enc { get; set; }
+        public string des { get; set; }
+
+        public Encriptador(string original)
+        {
+            try
+            {
+                using (RijndaelManaged myRijndael = new RijndaelManaged())
+                {
+                    myRijndael.Key = new byte[] { 201, 141, 160, 149, 24, 116, 240, 172, 124, 189, 18, 19, 108, 25, 66, 7, 173, 185, 109, 110, 221, 108, 219, 61, 164, 127, 138, 247, 93, 221, 63, 152 };
+                    myRijndael.IV = new byte[] { 242, 150, 76, 242, 34, 134, 218, 50, 74, 113, 72, 154, 153, 214, 226, 165 };
+                    byte[] arreglo = EncryptStringToBytes(original, myRijndael.Key, myRijndael.IV);
+                    enc = "";
+                    foreach (Byte b in arreglo)
+                    {
+                        enc += b + ",";
+                    }
+                    enc = enc.Substring(0, enc.Length - 1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("***************************** Error: {0}", e.Message);
+            }
+        }
+
+        public Encriptador(byte[] encrypted)
+        {
+            try
+            {
+                using (RijndaelManaged myRijndael = new RijndaelManaged())
+                {
+                    myRijndael.Key = new byte[] { 201, 141, 160, 149, 24, 116, 240, 172, 124, 189, 18, 19, 108, 25, 66, 7, 173, 185, 109, 110, 221, 108, 219, 61, 164, 127, 138, 247, 93, 221, 63, 152 };
+                    myRijndael.IV = new byte[] { 242, 150, 76, 242, 34, 134, 218, 50, 74, 113, 72, 154, 153, 214, 226, 165 };
+                    des = DecryptStringFromBytes(encrypted, myRijndael.Key, myRijndael.IV);
+                    Console.WriteLine("SALIDA!!!!!!!!!!!!!!!!!!! " + des);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("****************************** Error: {0}", e.Message);
+            }
+        }
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+    }
+}
--- Carrito_Compras/Carrito_Compras/Models/Usuario.cs
+++ Carrito_Compras/Carrito_Compras/Models/Usuario.cs
@@ -14,25 +14,21 @@ namespace Carrito_Compras.Models
         public string correo { get; set; }
         public string direccion { get; set; }
         public Rol rol { get; set; }
+        public string salida { get; set; }
 
         private bool connection_open;
         private MySqlConnection connection;
 
-        public Usuario()
-        {
-
-        }
-
-        public Usuario(int arg_id)
+        public Usuario(string arg_cor, string arg_pas)
         {
             Get_Connection();
-            id = arg_id;
+            correo = arg_cor;
             try
             {
                 MySqlCommand cmd = new MySqlCommand();
                 cmd.Connection = connection;
-                cmd.CommandText = string.Format("SELECT nombres_usuario, apellidos_usuario, password_usuario, correo_usuario," 
-                    + " direccion_usuario, rol_usuario FROM Usuario WHERE id_usuario = '{0}'", id);
+                cmd.CommandText = string.Format("SELECT nombres_usuario, apellidos_usuario, password_usuario, id_usuario,"
+                    + " direccion_usuario FROM Usuario WHERE correo_usuario = '{0}'", correo);
                 MySqlDataReader reader = cmd.ExecuteReader();
 
                 try
@@ -51,17 +47,27 @@ namespace Carrito_Compras.Models
                     else
                         password = null;
                     if (reader.IsDBNull(3) == false)
-                        correo = reader.GetString(3);
+                        id = int.Parse(reader.GetString(3));
                     else
-                        correo = null;
+                        id = -1;
                     if (reader.IsDBNull(4) == false)
                         direccion = reader.GetString(4);
                     else
                         direccion = null;
-                    if (reader.IsDBNull(5) == false)
-                        rol = new Rol(int.Parse(reader.GetString(5)));
-                    else
-                        rol = null;
+
+                    string[] aux = password.Split(',');
+                    byte[] arreglo = new byte[aux.Length];
+                    for (int i = 0; i < aux.Length; i++)
+                    {
+                        arreglo[i] = byte.Parse(aux[i]);
+                    }
+                    Encriptador enc = new Encriptador(arreglo);
+                    salida = enc.des;
+                    if (!salida.Equals(arg_pas))
+                    {
+                        nombres = "Error de contraseñas: se obtuvo " + password + " y se esperaba " + salida;
+                    }
+
                     reader.Close();
 
                 }
@@ -73,7 +79,6 @@ namespace Carrito_Compras.Models
                     reader.Close();
                     nombres = MessageString;
                     apellidos = password = correo = direccion = null;
-                    rol = null;
                 }
             }
             catch (MySqlException e)
@@ -82,16 +87,14 @@ namespace Carrito_Compras.Models
                     + e.ErrorCode + " - " + e.Message;
                 nombres = MessageString;
                 apellidos = password = correo = direccion = null;
-                rol = null;
             }
-          
+
             connection.Close();
         }
 
         private void Get_Connection()
         {
             connection_open = false;
-
             connection = new MySqlConnection();
             connection.ConnectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
             if (Open_Local_Connection())
@@ -103,7 +106,6 @@ namespace Carrito_Compras.Models
                 //					MessageBox::Show("No database connection connection made...\n Exiting now", "Database Connection Error");
                 //					 Application::Exit();
             }
-
         }
 
         private bool Open_Local_Connection()
@@ -117,6 +119,46 @@ namespace Carrito_Compras.Models
             {
                 return false;
             }
+        }
+
+        public void Agregar()
+        {
+            Encriptador enc = new Encriptador("admin");
+            string bob = enc.enc;
+            Get_Connection();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = string.Format("INSERT INTO `Ana1`.`Usuario` (`correo_usuario`,`nombres_usuario`,`apellidos_usuario`," +
+                    "`direccion_usuario`,`rol_usuario`,`password_usuario`,`foto_usuario`) VALUES (\"admin\",\"admin\"," +
+                    "\"admin\",\"----\",1,\"" + bob + "\",\"C://Ana1//foro.jpg\");");
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    reader.Read();
+                    nombres = "exito";
+                    Console.WriteLine(reader.ToString());
+                    reader.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    string MessageString = "***************** Read error occurred  / entry not found loading the Column details: "
+                        + e.ErrorCode + " - " + e.Message + "; \n\nPlease Continue";
+                    nombres = MessageString;
+                    reader.Close();
+                }
+            }
+            catch (MySqlException e)
+            {
+                string MessageString = "*********************** The following error occurred loading the Column details: "
+                    + e.ErrorCode + " - " + e.Message;
+                nombres = MessageString;
+            }
+
+            connection.Close();
         }
     }
 }
