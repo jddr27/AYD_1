19304c34cd92887b5b08b624bcf35980b61142bc
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2019-05-23 15:25:42 -0600

    FINAL PROYECTO AND MOCKUPS!

---
+++ Mockups/Carrito_Compras/Carrito_Compras/Models/Categoria.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Web;
+
+namespace Carrito_Compras.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+        private bool connection_open;
+        private MySqlConnection connection;
+
+        public Categoria()
+        {
+
+        }
+
+        public Categoria(int arg_id)
+        {
+            Get_Connection();
+            id = arg_id;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = string.Format("SELECT nombre_categoria FROM Categoria WHERE id_categoria = '{0}'", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    reader.Read();
+                    if (reader.IsDBNull(0) == false)
+                        nombre = reader.GetString(0);
+                    else
+                        nombre = null;
+                    reader.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    string MessageString = "Read error occurred  / entry not found loading the Column details: "
+                        + e.ErrorCode + " - " + e.Message + "; \n\nPlease Continue";
+                    //MessageBox.Show(MessageString, "SQL Read Error");
+                    reader.Close();
+                    nombre = MessageString;
+                }
+            }
+            catch (MySqlException e)
+            {
+                string MessageString = "The following error occurred loading the Column details: "
+                    + e.ErrorCode + " - " + e.Message;
+                nombre = MessageString;
+            }
+
+            connection.Close();
+        }
+
+        private void Get_Connection()
+        {
+            connection_open = false;
+
+            connection = new MySqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+            if (Open_Local_Connection())
+            {
+                connection_open = true;
+            }
+            else
+            {
+                //					MessageBox::Show("No database connection connection made...\n Exiting now", "Database Connection Error");
+                //					 Application::Exit();
+            }
+
+        }
+
+        private bool Open_Local_Connection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Mockups/Carrito_Compras/Carrito_Compras/Models/Usuario.cs
@@ -0,0 +1,122 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Web;
+
+namespace Carrito_Compras.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string password { get; set; }
+        public string correo { get; set; }
+        public string direccion { get; set; }
+        public Rol rol { get; set; }
+
+        private bool connection_open;
+        private MySqlConnection connection;
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int arg_id)
+        {
+            Get_Connection();
+            id = arg_id;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = string.Format("SELECT nombres_usuario, apellidos_usuario, password_usuario, correo_usuario," 
+                    + " direccion_usuario, rol_usuario FROM Usuario WHERE id_usuario = '{0}'", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    reader.Read();
+                    if (reader.IsDBNull(0) == false)
+                        nombres = reader.GetString(0);
+                    else
+                        nombres = null;
+                    if (reader.IsDBNull(1) == false)
+                        apellidos = reader.GetString(1);
+                    else
+                        apellidos = null;
+                    if (reader.IsDBNull(2) == false)
+                        password = reader.GetString(2);
+                    else
+                        password = null;
+                    if (reader.IsDBNull(3) == false)
+                        correo = reader.GetString(3);
+                    else
+                        correo = null;
+                    if (reader.IsDBNull(4) == false)
+                        direccion = reader.GetString(4);
+                    else
+                        direccion = null;
+                    if (reader.IsDBNull(5) == false)
+                        rol = new Rol(int.Parse(reader.GetString(5)));
+                    else
+                        rol = null;
+                    reader.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    string MessageString = "Read error occurred  / entry not found loading the Column details: "
+                        + e.ErrorCode + " - " + e.Message + "; \n\nPlease Continue";
+                    //MessageBox.Show(MessageString, "SQL Read Error");
+                    reader.Close();
+                    nombres = MessageString;
+                    apellidos = password = correo = direccion = null;
+                    rol = null;
+                }
+            }
+            catch (MySqlException e)
+            {
+                string MessageString = "The following error occurred loading the Column details: "
+                    + e.ErrorCode + " - " + e.Message;
+                nombres = MessageString;
+                apellidos = password = correo = direccion = null;
+                rol = null;
+            }
+          
+            connection.Close();
+        }
+
+        private void Get_Connection()
+        {
+            connection_open = false;
+
+            connection = new MySqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+            if (Open_Local_Connection())
+            {
+                connection_open = true;
+            }
+            else
+            {
+                //					MessageBox::Show("No database connection connection made...\n Exiting now", "Database Connection Error");
+                //					 Application::Exit();
+            }
+
+        }
+
+        private bool Open_Local_Connection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
